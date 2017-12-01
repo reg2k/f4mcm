0e767e4b6b6cbc92a2e17ea03f2fdd105281bd19
reg2k <23437153+reg2k@users.noreply.github.com>
2017-12-01 08:52:19 +0000

    Update address for v1.10.50.

--- src/Config.h
+++ src/Config.h
@@ -5,7 +5,7 @@
 // Plugin Information
 //-----------------------
 #define PLUGIN_VERSION              4
-#define PLUGIN_VERSION_STRING       "1.22"
+#define PLUGIN_VERSION_STRING       "1.22b"
 #define PLUGIN_NAME_SHORT           "F4MCM"
 #define PLUGIN_NAME_LONG            "Mod Configuration Menu"
 #define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
@@ -14,7 +14,7 @@
 // Addresses
 #if SUPPORTED_RUNTIME_VERSION == RUNTIME_VERSION_1_10_50
     #define Addr_ExecuteCommand         0x0125AF00
-    #define Addr_ProcessUserEvent_Check 0x0210F120
+    #define Addr_ProcessUserEvent_Check 0x0210F1AC
     #define Addr_GetPropertyInfo        0x02718460
 
 #elif SUPPORTED_RUNTIME_VERSION == RUNTIME_VERSION_1_10_40
