199d64f426f5e214aacc6cc9f6cb8116fcc8d490
Unknown <gr-el@ya.ru>
2018-07-26 21:13:53 +0500

    additional updates for 1 10 106 build

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import os, sys
 BUILD_DIR        = 'build'
 PROJECT_DIR      = 'src'
 PLATFORM_TOOLSET = 'v140'
-F4SE_REVISION    = 'tags/v0.6.10'
+F4SE_REVISION    = 'tags/v0.6.11'
 
 # Run build tools
 buildOK = os.system('python tools/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
