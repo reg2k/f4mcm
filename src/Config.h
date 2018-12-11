7ecff49f21f3574143a0fbb009be1ca41db6c7e7
Unknown <gr-el@ya.ru>
2018-12-11 20:18:26 +0500

    latest swf

--- src/Config.h
+++ src/Config.h
@@ -5,7 +5,7 @@
 // Plugin Information
 //-----------------------
 #define PLUGIN_VERSION              6
-#define PLUGIN_VERSION_STRING       "1.32"
+#define PLUGIN_VERSION_STRING       "1.34"
 #define PLUGIN_NAME_SHORT           "F4MCM"
 #define PLUGIN_NAME_LONG            "Mod Configuration Menu"
 #define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
